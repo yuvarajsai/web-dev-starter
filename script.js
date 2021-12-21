048b8af034ab33cba8f07e58dd1691e6c9c084f2
Yuvaraj Sai <yuvarajsait@gmail.com>
2021-12-21 13:46:49 +0000

    DOM done

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
-alert("Hello world!");
+let numbers = [2,42.58,2];
+console.log(numbers);
+
+//DOM//
+let salary = Symbol();
+let age = 12;
+
+console.log(Math.PI);
+
+let person = {
+    name: "Yuvi",
+    dob: 2002,
+    [salary]: '40k',
+};
+
+const incrementAge = (a) => {
+    a++;
+};
+
+const incrementDOB = (person) => {
+    person.dob+=2;
+}
+
+incrementDOB(person);
+showMessage(person.dob);
+
+
+
+// incrementAge(age);
+// showMessage(age);
+
+// console.log(person.salary);
+
+
+
+
+
+// description: function(){
+//     showMessage("Even certainity of uncertainity is uncertain!");
+// },
+// favFood: function(){
+//     showMessage(this.name);
+// }
+
+
+// console.log(person.name);
+// console.log(person.salary);
+// console.log(person.description);
+
+
--- utils.js
+++ utils.js
@@ -1,1 +1,8 @@
-alert("Holy crap!");
+let showMessage = (message) => {
+    document.getElementById('message').textContent = message;
+}
+
+let button = document.getElementById('btn');
+button.addEventListener('click', function(){
+    console.log('Submitted!');
+})
